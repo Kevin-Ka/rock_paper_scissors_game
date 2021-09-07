a76e39ac359eb5448d4213a2b07fd879c852f0e8
Kevin-Ka <k_kalmbach95@web.de>
2021-09-07 18:25:31 +0200

    git commit -m

--- script.js
+++ script.js
@@ -12,57 +12,94 @@ function computerPlay() {
     return comPlay
 }
 
+let userScore = 0;
 
-function game() {
-    let userScore = 0;
-    let comScore = 0;
-    for (let i = 0;i < 5; i++){
-        let comChoice = computerPlay();
-        let userChoice = window.prompt("Up for a round of Rock, Paper, Scissors? \nMake your Choice!").toLowerCase();
-        console.log("You chose " + userChoice)
-        console.log("Computer chose " + comChoice)
-        if (userChoice = "rock"){
-            switch (comChoice){
-                case "paper":
-                    comScore++;
-                    break;
-                case "scissors":
-                    userScore++;
-                    break;
-                default:
-                    break;
-            }
-        } else if (userChoice = "paper"){
-            switch (comChoice){
-                case "scissors":
-                    comScore++;
-                    break;
-                case "rock":
-                    userScore++;
-                    break;
-                default:
-                    break;
-            }
-        } else if (userChoice = "scissors"){
-            switch (comChoice){
-                case "rock":
-                    comScore++;
-                    break;
-                case "paper":
-                    userScore++;
-                    break;
-                default:
-                    break;
-            }
-            }
+let comScore = 0;
+
+function checkScore() {
+    if(userScore == 5 || comScore == 5){
+        if(userScore > comScore){
+            alert('YOU WIN!');
+            resetFunc();
+        }else{
+            alert('YOU LOSE!');
+            resetFunc();
         }
-        if (userScore > comScore) {
-            return "You have scored " + userScore +" times, while the computer scored " + comScore + " times \nYou won!" 
-        } else if (userScore < comScore) {
-            return "You have scored " + userScore +" times, while the computer scored " + comScore + " times \nYou lost!" 
-        } else if(userScore == comScore) {
-            return "It's a tie. Play again!"
-        }
+    }
+};
+
+function scoreFunc(){
+    if(document.contains(document.getElementById("scoreData"))){
+        document.getElementById("scoreData").remove();
+    } 
+    const score = document.querySelector('#score');
+    const scoreData = document.createElement('div');
+    scoreData.classList.add('scoreData');
+    scoreData.textContent = "User= " + userScore + "  Computer= "+ comScore;
+
+    score.appendChild(scoreData);
+    scoreData.setAttribute("id", "scoreData")
+
+    checkScore();
+
 }
 
-console.log(game())
+let rock = document.getElementById("rock");
+rock.addEventListener("click",playerRock);
+
+function playerRock() {
+    let comChoice = computerPlay();
+    switch (comChoice){
+        case "paper":
+            comScore++;
+            break;
+        case "scissors":
+            userScore++;
+            break;
+        default:
+            break;
+    }
+    scoreFunc();
+}
+
+let paper = document.getElementById("paper");
+paper.addEventListener("click",playerPaper);
+
+function playerPaper() {
+    let comChoice = computerPlay();
+    switch (comChoice){
+        case "rock":
+            userScore++;
+            break;
+        case "scissors":
+            comScore++;
+            break;
+        default:
+            break;
+    }
+    scoreFunc();
+}
+
+let scissors = document.getElementById("scissors");
+scissors.addEventListener("click",playerScissors);
+
+function playerScissors() {
+    let comChoice = computerPlay();
+    switch (comChoice){
+        case "paper":
+            userScore++;
+            break;
+        case "rock":
+            comScore++;
+            break;
+        default:
+            break;
+    }
+    scoreFunc();
+}
+
+function resetFunc() {
+    userScore=0;
+    comScore=0
+    document.getElementById("scoreData").remove();
+}
